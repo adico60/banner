9db54ea2eb3dc87c3b7e8fd70f707228f28634c3
YaNn <yann.berton@Yann-PC.domaine.local>
2015-05-28 17:45:13 +0200

    Initial COmmit

---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "pradinestemplate".
+ *
+ * Auto generated 23-08-2014 13:00
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Banner',
+	'description' => 'Banner',
+	'category' => 'misc',
+	'version' => '0.0.1',
+	'state' => 'beta',
+	'uploadfolder' => true,
+	'createDirs' => '',
+	'clearcacheonload' => true,
+	'author' => 'Yann Bogdanovic feat Yann Berton',
+	'author_email' => 'yann.bogdanovic@cdgfpt46.fr;yann.berton@adico.fr',
+	'author_company' => 'CDGFPT 46/ADICO',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '6.2.0-6.2.99',
+			'bootstrap_package' => '6.2.0-6.2.99',
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
